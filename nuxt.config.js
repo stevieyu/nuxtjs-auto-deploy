c0c0548937bfcd73bd3555bfba56e2aa9e560af1
Stevie <yzm629@sina.cn>
2019-09-18 10:21:45 +0800

    build: update

--- nuxt.config.js
+++ nuxt.config.js
@@ -33,7 +33,6 @@ export default {
   ** Nuxt.js modules
   */
   modules: [
-    '@nuxtjs/eslint-module'
   ],
   /*
   ** Build configuration
@@ -44,5 +43,8 @@ export default {
     */
     extend(config, ctx) {
     }
-  }
+  },
+  server: {
+    host: '0.0.0.0',
+  },
 }
